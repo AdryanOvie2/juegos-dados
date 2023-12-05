e956d51865d543ab887d88dc61dc70b11855cf67
AdryanOvie2 <adryanovie2@gmail.com>
2023-12-05 13:13:23 +0100

    fixed game+

--- juego_dado.py
+++ juego_dado.py
@@ -7,20 +7,22 @@ def juego_dados():
     print("===========================")
     print("       Juego de Dados      ")
     print("===========================")
-    num_partidas = int(input("\n¿Cuántas partidas van a jugar: "))
+    num_partidas = 0
     lances = 5
     lista_num_player1 = []
     lista_num_player2 = []
     valores_dado = [1, 2, 3, 4 ,5 ,6]
-
+    i = 1;
     if num_partidas == 0:
         print("\n¡Como mínimo se debe jugar una partida!\n")
         num_partidas = int(input("\n¿Cuántas partidas desea: "))
     
-    player1 = input("\nNombre del Player 1: ")
-    player2 = input("\nNombre del Player 2: ")
-
-    while num_partidas != 0:
+        player1 = input("\nNombre del Player 1: ")
+        player2 = input("\nNombre del Player 2: ")
+    
+    
+    while i <= num_partidas:
+        print(f"\nPartida {i}:")
         while lances != 0:
             num_dados_player1 = random.choice(valores_dado)
             num_dados_player2 = random.choice(valores_dado)
@@ -35,14 +37,23 @@ def juego_dados():
         print(f"\nTirada de {player1}: {lista_num_player1} => {suma_player1} puntos.")
         print(f"\nTirada de {player2}: {lista_num_player2} => {suma_player2} puntos.")
 
-        num_partidas -= 1
-    
-    if suma_player1 == suma_player2:
-        print("\n¡Hubo empate!")
-    elif suma_player1 > suma_player2:
-        print(f"\n¡Ha ganado {player1}!")
+        lances = 5
+        lista_num_player1 = []
+        lista_num_player2 = []
+        
+        if suma_player1 == suma_player2:
+            print("\n¡Hubo empate!")
+        elif suma_player1 > suma_player2:
+            print(f"\n¡Ha ganado {player1}!")
+        else:
+            print(f"\n¡Ha ganado {player2}!")
+        
+        i += 1
+
+    if i <= num_partidas:
+        juego_dados()
     else:
-        print(f"\n¡Ha ganado {player2}!")
+        return 0
 
 
 juego_dados()
